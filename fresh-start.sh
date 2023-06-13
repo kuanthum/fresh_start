0d7726f77e3d7b3091dfb47a01ef76b9eb139dce
agu <agustin.ojeda.90@gmail.com>
2023-06-12 21:05:36 -0300

    aded init.vim

--- fresh-start.sh
+++ fresh-start.sh
@@ -6,6 +6,7 @@ apt_packages=(
 	"neovim"
 	"pip"
 	"curl"
+	"tmux"
 )
 
 # Install apt packages func
@@ -28,7 +29,7 @@ curl_packages=(
 		echo 'source ~/.bashrc"
 	["teamviewer"]="
 		wget -P ~/Downloads https://download.teamviewer.com/download/linux/teamviewer_amd64.deb &&
-		sudo apt install ~/Downloads/teamviewer_amd64.deb"	
+		sudo apt install ~/Downloads/teamviewer_amd64.deb"
 )
 
 # Install with curl packages func
@@ -49,5 +50,23 @@ for cmd in "${!curl_packages[@]}"; do
 	execute_command "$cmd" "${curl_packages[$cmd]}"
 done
 
+# Raw download 
+file_list=(
+        ["init.vim"]=("https://raw.githubusercontent.com/your-username/your-repo/main/init.vim" "$HOME/.config/nvim/init.vim")
+)
+
+for file in "${!file_list[@]}"; do
+    params=("${file_list[$file]}")
+    url="${params[0]}"
+    destination="${params[1]}"
+
+    if [ -e "$destination" ]; then
+        echo "File $file already exists at $destination. Skipping download."
+    else
+        echo "Downloading $file to $destination..."
+        curl -o "$destination" "$url"
+    fi
+done
+	
 echo "Installation completed."
 
